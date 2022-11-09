1cfb456b6461db0cd1161afef2669c4f9218ec15
Denis Borodkin <den_borodkin@inbox.ru>
2022-11-09 12:13:16 +0300

    The money replenishment unit is ready

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,5 @@
 
+
 const newLogoutButton = new LogoutButton();
 newLogoutButton.action = () => {
 	ApiConnector.logout(data => {
@@ -9,6 +10,29 @@ newLogoutButton.action = () => {
 }
 ApiConnector.current(data => {
 	if (data.success) {
-		ProfileWidget.showProfile(data);
+		ProfileWidget.showProfile(data.data);
 	}
-})
+});
+const newRatesBoard = new RatesBoard();
+let timerId = setInterval(() => {
+	ApiConnector.getStocks(data => {
+		if (data.success) {
+			newRatesBoard.clearTable();
+			newRatesBoard.fillTable(data.data)
+		}
+	})
+}, 60000);
+const newMoneyManager = new MoneyManager();
+newMoneyManager.addMoneyCallback = data => {
+	ApiConnector.addMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			newMoneyManager.setMessage(response.success, 'Пополнение выполненно успешно');
+		}
+		else {
+			newMoneyManager.setMessage(data.error, response.error);
+		}
+	});
+};
+
+
